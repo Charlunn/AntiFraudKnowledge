aeb88c2a4ff1231b4dd26caa5995e04f8c49cc47
CharlunShary <charlunn@icloud.com>
2025-05-17 16:51:43 +0800

    init user with django

--- backend/KnowledgeBackend/settings.py
+++ backend/KnowledgeBackend/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'graph_api'
+    'graph_api',
+    'users'
 ]
 TEMPLATES = [
     {
